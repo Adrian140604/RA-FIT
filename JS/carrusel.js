3a611d0039c4d034004497b7c584605e2909b5e1
Parrot Live user <user@localhost.localdomain>
2024-05-20 16:35:45 +0000

    Carrusel arreglado para linux

--- JS/carrusel.js
+++ JS/carrusel.js
@@ -1,7 +1,7 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     const carouselImages = document.querySelector('.carousel-images');
     let imageIndex = 0;
+    const totalImages = carouselImages.children.length;
 
     function nextImage() {
         // Calcula el ancho de una imagen
@@ -14,16 +14,16 @@ document.addEventListener("DOMContentLoaded", function() {
         carouselImages.style.transform = `translateX(${translateX}px)`;
 
         // Si llegamos al final, vuelve al principio
-        if (imageIndex === carouselImages.children.length - 1) {
-            imageIndex = 0;
+        if (imageIndex === totalImages) {
             setTimeout(() => {
                 carouselImages.style.transition = 'none';
                 carouselImages.style.transform = `translateX(0)`;
+                imageIndex = 0;
                 // Establece un pequeño retraso para reiniciar la transición
                 setTimeout(() => {
                     carouselImages.style.transition = 'transform 0.8s ease';
                 }, 10);
-            }, 500);
+            }, 800);
         }
     }
 
